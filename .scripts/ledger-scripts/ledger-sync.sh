1633dcbd11981b07bfc30b27561bccc5136e94d8
Patrick Skiba <pskiba@posteo.net>
2019-05-04 06:08:42 -0400

    Adding prompts with questions to the ledger script

--- .scripts/ledger-scripts/ledger-sync.sh
+++ .scripts/ledger-scripts/ledger-sync.sh
@@ -1,1 +1,1 @@
-diff act.csv act2.csv | grep -E '^>' | sed 's/>\s//g' | awk -F ',' '{ print $1","$3","$4","$6 }'
+node ledger-prompt.js "$(diff act.csv act2.csv | grep -E '^>' | sed 's/>\s//g' | awk -F ',' '{ print $1","$3","$4","$6 }')"
