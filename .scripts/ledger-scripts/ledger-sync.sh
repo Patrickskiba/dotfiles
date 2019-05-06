b751edc2270457cf019ac0e07eca0d38a03117b8
Patrick Skiba <pskiba@posteo.net>
2019-05-06 18:59:06 -0400

    Converting ledger script to only bash for simplicity"

--- .scripts/ledger-scripts/ledger-sync.sh
+++ .scripts/ledger-scripts/ledger-sync.sh
@@ -1,1 +1,10 @@
-node ledger-prompt.js "$(diff act.csv act2.csv | grep -E '^>' | sed 's/>\s//g' | awk -F ',' '{ print $1","$3","$4","$6 }')"
+IFS="\\"
+transactions=($(diff act.csv act2.csv | grep -E '^>' | sed 's/>\s//g' | awk -F ',' '{ print $1","$3","$4","$6"\\" }'))
+
+for tran in "${transactions[@]}"
+do
+    echo $tran
+    read -p "Was this purchase made by Erin, Patrick or Both? (E/P/B) : " who
+    echo $yn
+done
+
