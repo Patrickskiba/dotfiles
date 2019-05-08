1d6943245f02e6550b1ee4066688501f13cc01c8
Patrick Skiba <pskiba@posteo.net>
2019-05-08 16:18:52 -0400

    Ask for expense category and account

--- .scripts/ledger-scripts/ledger-sync.sh
+++ .scripts/ledger-scripts/ledger-sync.sh
@@ -4,13 +4,22 @@ transactions=($(diff act.csv act2.csv | grep -E '^>' | sed 's/>\s//g' | awk -F '
 for tran in "${transactions[@]}"
 do
     echo $tran
-    read -p "Was this purchase made by Erin, Patrick or Both? (E/P/B) : " who
-    case $who in
+    read -p "Was this purchase made by Erin, Patrick or Both? (E/P/B) : " WHO
+    case $WHO in
         [Ee]* ) echo "It was Erin!";;
         [Pp]* ) echo "It was Patrick!";;
         [Bb]* ) echo "Both did it";;
     esac
 
-    CATEGORIY=$(awk 'BEGIN { print "Grocery\nResturant\nAlcohol\nBills\nRent\nEntertainment\nHome"}' | fzy)
+    CATEGORIY=$(awk 'BEGIN { print "Grocery\nResturant\nAlcohol\nBills\nRent\nEntertainment\nHome\nOther"}' | fzy)
+
+    case $CATEGORIY in
+        Other) read -p "Enter Category : " CATEGORIY;;
+    esac
+
+    CreditCard=$(awk 'BEGIN { print "CapitalOne\nAmex\nChase\nTravelAmex\nMarriott"}' | fzy)
+
+    echo $CreditCard
+
 done
 
